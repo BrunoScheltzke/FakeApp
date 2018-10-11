ee16e759d5a6a0380dc8c721964bd8dafd12eb74
Bruno Scheltzke <brunofonten@gmail.com>
2018-10-11 10:20:58 -0300

    added reliability indexes

--- FakeApp/FakeApp/Model/News.swift
+++ FakeApp/FakeApp/Model/News.swift
@@ -19,12 +19,16 @@ enum ReliabilityIndex: Int {
     case fake = 0
     case fact = 1
     case neutral = 2
+    case fakeIsh = 3
+    case trueIsh = 4
     
     var asString: String {
         switch self {
         case .fake: return "Fake"
         case .fact: return "Fato"
         case .neutral: return "Neutro"
+        case .fakeIsh: return "Pode ser Fake"
+        case .trueIsh: return "Pode ser Fato"
         }
     }
     
@@ -33,6 +37,8 @@ enum ReliabilityIndex: Int {
         case .fake: return "Baseado na opinião dos usuários, concluímos que essa notícia é falsa."
         case .fact: return "Baseado na opinião dos usuários, concluímos que essa notícia é verdadeira."
         case .neutral: return "Não temos votos suficientes para determinar a veracidade dessa notícia. Vote e ajude!"
+        case .fakeIsh: return "A maioria das pessoas votou Fake, mas a reputação delas não é tão alta, então não podemos afirmar."
+        case .trueIsh: return "A maioria das pessoas votou Fato, mas a reputação delas não é tão alta, então não podemos afirmar."
         }
     }
     
@@ -41,6 +47,8 @@ enum ReliabilityIndex: Int {
         case .fake: return #colorLiteral(red: 0.6784536895, green: 0.04439244277, blue: 0, alpha: 1)
         case .fact: return #colorLiteral(red: 0.001842024725, green: 0.6487192119, blue: 0.002858069079, alpha: 1)
         case .neutral: return #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+        case .fakeIsh: return #colorLiteral(red: 0.6964779598, green: 0.2887971449, blue: 0.2610646096, alpha: 1)
+        case .trueIsh: return #colorLiteral(red: 0.4457544687, green: 0.6487192119, blue: 0.4069307863, alpha: 1)
         }
     }
 }

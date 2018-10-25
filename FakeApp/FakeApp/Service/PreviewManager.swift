283523af04a381cb47dbeccf3a2d101d8b49a565
Bruno Scheltzke <brunofonten@gmail.com>
2018-10-25 20:44:59 -0300

    adds timeout for preview

--- FakeApp/FakeApp/Service/PreviewManager.swift
+++ FakeApp/FakeApp/Service/PreviewManager.swift
@@ -10,10 +10,18 @@ import Foundation
 import SwiftLinkPreview
 
 class PreviewManager {
-    let slp = SwiftLinkPreview(session: URLSession.shared,
+    let slp: SwiftLinkPreview
+    
+    init() {
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.timeoutIntervalForRequest = 07.0
+        sessionConfig.timeoutIntervalForResource = 07.0
+        let session = URLSession(configuration: sessionConfig)
+        slp = SwiftLinkPreview(session: session,
                                workQueue: SwiftLinkPreview.defaultWorkQueue,
                                responseQueue: DispatchQueue.main,
                                cache: DisabledCache.instance)
+    }
     
     func getPreview(of url: String, completion: @escaping((title: String, portal: String)?, Error?) -> Void) {
         slp.preview(url, onSuccess: { result in
